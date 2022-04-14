6c3f27e07a6befd384063dd6419cb7ce98d673ae
WIN-N4JJTQ33THC\Administrator <zhuzichu520@gmail.com>
2022-04-14 17:54:46 +0800

    update

--- pack/binarycreator.py
+++ pack/binarycreator.py
@@ -1,7 +1,28 @@
 import os
+import shutil
+from greenlet import greenlet
 
-path = "D:\\Qt\\QtIFW-4.3.0\\bin\\"
-binarycreator = path+"binarycreator.exe"
-print(binarycreator)
-status = os.system(binarycreator+" --online-only -c config/config.xml -p packages installer")
-input()
+path_ifw = "D:\\Qt\\QtIFW-4.3.0\\bin\\"
+path_binarycreator = path_ifw + "binarycreator.exe"
+exe_install = "installer.exe"
+
+
+def main():
+    print("开始生成install.exe")
+    if os.path.exists(exe_install):
+        shutil.rmtree(exe_install, True)
+    print(exe_install)
+    state = g_binarycreator.switch()
+    if state == 1:
+        print("install.exe已生成，随意按键退出程序")
+        input()
+
+
+def binarycreator():
+    os.system(path_binarycreator + " --online-only -c config/config.xml -p packages installer")
+    g_main.switch(1)
+
+
+g_main = greenlet(main)
+g_binarycreator = greenlet(binarycreator)
+g_main.switch()
--- pack/repogen_update.py
+++ pack/repogen_update.py
@@ -1,7 +1,7 @@
 import os
 
 path = "D:\\Qt\\QtIFW-4.3.0\\bin\\"
-repogen = path+"repogen.exe"
+repogen = path + "repogen.exe"
 print(repogen)
-status = os.system(repogen+" --update-new-components -p packages_update repository")
+status = os.system(repogen + " --update-new-components -p packages_update repository")
 input()
